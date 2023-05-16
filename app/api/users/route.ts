f98d9f84751e7964fcf23b41a0406e599ce8ce00
Matt <director@acidgambit.com>
2023-05-16 10:00:35 -0400

    connect db to auth

---
+++ app/api/users/route.ts
@@ -0,0 +1,13 @@
+import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const users = await prisma.user.findMany();
+
+  console.log(users);
+
+  // return NextResponse.json(users);
+  return new Response(JSON.stringify(users), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
