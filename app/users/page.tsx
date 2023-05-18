bcc98930434974087f634ec9727d53147de55df5
Matt <director@acidgambit.com>
2023-05-18 12:29:23 -0400

    add loading + error ui to users route

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,6 +2,8 @@ import UserCard from "@/components/UserCard";
 import { prisma } from "@/lib/prisma";
 
 export default async function Users() {
+  // test error ui
+  // throw new Error("this is an error");
   const users = await prisma.user.findMany();
 
   return (
